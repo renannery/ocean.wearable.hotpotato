dfeb7d4bc6a3933075ba8842cca00d88c641593c
Renan Nery <renannery@gmail.com>
2015-12-07 17:40:47 -0200

    Changing planes, lets start developing a simple YO XMPP client

--- SendPotato/js/main.js
+++ SendPotato/js/main.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     // TODO:: Do your initialization job
-    Parse.initialize("1YUyKkLv9N8FxHAp8ivS6WluD8JCbxYTm4JK8bgW", "7SDxfjnVqLUOPoAUlURxCvKIYfgSJTv2mosIqoKL");
-
+    
+    var jid;
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if (e.keyName == "back")
@@ -10,25 +10,95 @@ window.onload = function() {
             } catch (ignore) {}
     });
 
-    document.getElementById('content_text')
-        .addEventListener('click', function() {
-            var potatoes = ["Batata", "Joelson", "Pafuncio", "Richarlyson", "Mr potato"];
-            for (i = 0; i < potatoes.length; i++) {
-                var Potato = Parse.Object.extend("Potato");
-                var potato = new Potato();
-                potato.set("name", potatoes[i]);
-                potato.set("lastLocation", new Parse.GeoPoint(30, 30));
-                potato.save(null, {
-                    success: function(potato) {
-                        // Execute any logic that should take place after the object is saved.
-                        alert("Potato created");
-                    },
-                    error: function(potato, error) {
-                        // Execute any logic that should take place if the save fails.
-                        // error is a Parse.Error with an error code and message.
-                        alert('Failed to create new object, with error code: ' + error.message);
-                    }
-                });
-            }
+    $(document).ready(function() {
+        $("#connectBut").click(function() {
+
+            jid = $('input[name="user"]:checked').val();
+            var password = "Senha2015"
+            var logContainer = $("#log");
+            var contactList = $("#contacts");
+            var messageTo = "";
+
+            //An example of bosh server. This site is working but it can change or go down.
+            //If you are going to have a production site, you must install your own BOSH server
+            var url = "http://jabber.rootbash.com:7070/http-bind/";
+            $.xmpp.connect({
+                url: url,
+                jid: jid,
+                password: password,
+                onConnect: function() {
+                    alert("Connected");
+                    logContainer.html("Connected");
+                    $.xmpp.setPresence(null);
+                },
+                onPresence: function(presence) {
+
+                    var contact = $("<li>");
+                    messageTo = presence.from;
+                    contact.append("<a href='javascript:void(0)'>" + presence.from + "</a>");
+                    contact.find("a").click(function() {
+                        var id = MD5.hexdigest(presence.from);
+                        var conversation = $("#" + id);
+                        if (conversation.length == 0)
+                            openChat({
+                                to: presence.from
+                            });
+                    });
+                    contactList.append(contact);
+                },
+                onDisconnect: function() {
+                    alert("Disconnected");
+                    logContainer.html("Disconnected");
+                },
+
+                onMessage: function(message) {
+                    var user = message.from.split("@", 1);
+                    alert(user + "\n" + message.body);
+                },
+                onError: function(error) {
+                    alert(error.error);
+                }
+            });
         });
+
+        $("#disconnectBut").click(function() {
+            $.xmpp.disconnect();
+        });
+
+        $("#sendMessage").click(function() {
+            var messageTo;
+            if (jid == "renannery10") {
+                messageTo = "ursinho@jabber.rootbash.com";
+            } else {
+                messageTo = "renannery10@jabber.rootbash.com";
+            }
+            
+            $.xmpp.sendMessage({
+                body: "Yo",
+                to: messageTo
+            });
+        });
+
+    });
+
+    function openChat(options) {
+        var id = MD5.hexdigest(options.to);
+
+        var chat = $("<div style='border: 1px solid #000000; float:left' id='" + id + "'><div style='border: 1px solid #000000;'>Chat with " + options.to + "</div><div style='height:150px;overflow: auto;' class='conversation'></div><div><input type='text' /><button>Send</button></div></div>");
+        var input = chat.find("input");
+        var sendBut = chat.find("button");
+        var conversation = chat.find(".conversation");
+        sendBut.click(function() {
+            $.xmpp.sendMessage({
+                to: options.to,
+                body: input.val()
+            });
+            conversation.append("<div>" + $.xmpp.jid + ": " + input.val() + "</div>");
+            input.val("");
+        });
+        $("body").append(chat);
+    }
+
+
+
 };
